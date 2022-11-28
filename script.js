8362d70d00bc2fd3c74e75d590acad272356c63b
YAHOR-RUDZIANKOU <rudenkov.egor03@mail.ru>
2022-11-29 00:21:17 +0300

    changes

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
-let button = document.querySelector(".button");
-let input = document.querySelector(".input");
-button.addEventListener("click", addList);
+const button = document.querySelector("#button");
+const input = document.querySelector("#input");
+const list = document.querySelector("#list");
+button.addEventListener("click", addElements);
 let increment = 0;
-function addList() {
-    let value = input.value.trim();
+let inc = 0;
+
+function addElements() {
+    input.focus();
+    for (let i = 0; i < input.value.length; i++) {
+        if (input.value[i] === " ") {
+            inc++;
+        }
+    }
+    if (inc > 5) {
+        input.classList.add("mistake");
+    }
+    const value = input.value.trim();
     if (input.value.length > 25) {
-        alert("Ошибка");
-        input.value = "";
+        input.classList.add("mistake");
+
     } else if (value) {
-        input.value = "";
-        let li = document.createElement("li");
+        input.classList.remove("mistake");
+        const li = document.createElement("li");
         li.innerText = value;
         increment++;
-        if (increment % 2 === 0) {
+        if (!(increment % 2)) {
             li.classList.add("red");
         } else li.classList.add("green");
-        document.body.append(li)
+        list.append(li)
     }
+    input.value = "";
 }
+
 addEventListener("keydown", buttonPress);
 
 function buttonPress(event) {
     if (event.code === "Enter") {
-        addList();
+        addElements();
     }
 }
