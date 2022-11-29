7388a8b48e7239392fa59bbb22fcf6309afca5c8
YAHOR-RUDZIANKOU <rudenkov.egor03@mail.ru>
2022-11-29 20:12:10 +0300

    fixed remax in pull-recvest

--- script.js
+++ script.js
@@ -3,22 +3,12 @@ const input = document.querySelector("#input");
 const list = document.querySelector("#list");
 button.addEventListener("click", addElements);
 let increment = 0;
-let inc = 0;
 
 function addElements() {
     input.focus();
-    for (let i = 0; i < input.value.length; i++) {
-        if (input.value[i] === " ") {
-            inc++;
-        }
-    }
-    if (inc > 5) {
-        input.classList.add("mistake");
-    }
     const value = input.value.trim();
-    if (input.value.length > 25) {
+    if (value.length > 25 || value.length === 0) {
         input.classList.add("mistake");
-
     } else if (value) {
         input.classList.remove("mistake");
         const li = document.createElement("li");
@@ -26,16 +16,16 @@ function addElements() {
         increment++;
         if (!(increment % 2)) {
             li.classList.add("red");
-        } else li.classList.add("green");
-        list.append(li)
+        } else {
+            li.classList.add("green");
+        }
+        list.appendChild(li)
     }
     input.value = "";
 }
 
-addEventListener("keydown", buttonPress);
-
-function buttonPress(event) {
+addEventListener("keydown", buttonPress=event=>{
     if (event.code === "Enter") {
         addElements();
     }
-}
+});
